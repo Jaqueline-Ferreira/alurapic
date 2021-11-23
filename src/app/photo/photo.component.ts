4b7ffd9eaf6206eb3dfcbea33ce5cc7bc38adbaa
unknown <ferreirasousajaqueline@gmail.com>
2021-11-23 19:04:42 -0300

    fazendo algumas modificações

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,3 +1,14 @@
-export class photo { 
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'ap-photo',
+    templateUrl: 'photo.component.html'
+})
+
+export class PhotoComponent { 
+
+    description = 'totoro';
+
+    url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYEidbP7x-1FMAgZwbEhcsYAef0idV2X14mw&usqp=CAU'
 
 }
