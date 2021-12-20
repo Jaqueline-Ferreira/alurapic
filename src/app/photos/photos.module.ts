725f217756e6caffafbea24c55ec6a86860b517a
unknown <ferreirasousajaqueline@gmail.com>
2021-12-20 17:28:31 -0300

    criando rotas

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 import { PhotoComponent } from "./photo/photo.component";
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { CommonModule } from "@angular/common";
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule ({
-    declarations: [ PhotoComponent, PhotoListComponent ],
+    declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent ],
     imports: [
     HttpClientModule,
     CommonModule
