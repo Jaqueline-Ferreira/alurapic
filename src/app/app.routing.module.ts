ae165941f15b0bf0534b45cab9604f0b52bf2c38
unknown <ferreirasousajaqueline@gmail.com>
2022-01-04 15:57:55 -0300

    resolver e subscribe

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,10 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
-    { path: 'user/:userName', component: PhotoListComponent },
-    { path: 'p/add', component: PhotoFormComponent }
+    {
+         path: 'user/:userName',
+        component: PhotoListComponent, 
+        resolve: {
+        photos: PhotoListResolver} 
+    },
+    { 
+        path: 'p/add', 
+        component: PhotoFormComponent 
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
 ];
 
 @NgModule({
