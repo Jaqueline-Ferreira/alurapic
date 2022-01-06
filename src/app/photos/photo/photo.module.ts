30736548b7262f92cfbeac0c7e710ea2b51d35bb
unknown <ferreirasousajaqueline@gmail.com>
2022-01-06 00:54:51 -0300

    Component container e ng-content

--- src/app/photos/photo/photo.module.ts
+++ src/app/photos/photo/photo.module.ts
@@ -1,7 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { PhotoComponent } from './photo.component';
 
 @NgModule({
-    declarations: [PhotoComponent]
+    declarations: [PhotoComponent],
+    imports: [ CommonModule,
+        HttpClient 
+    ]
 })
 export class PhotoModule { }
